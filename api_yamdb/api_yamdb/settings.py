aba23d7f4d5c0b0456673628e92e510e2031cb2e
Daniil Pautov <93451214+TomatoInOil@users.noreply.github.com>
2022-08-14 12:51:28 +0300

    Enabling debug mode.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='a-V3rY-S3cr3t-k3y')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', os.getenv('HOST')]
 
